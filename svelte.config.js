39b5528b8144fbc2bf24c25d3171f1bc47a64ead
Qmm <1259598502@qq.com>
2024-07-13 21:00:36 +0800

    小修改

--- svelte.config.js
+++ svelte.config.js
@@ -2,7 +2,7 @@
  * @Author: Qmm 1259598502@qq.com
  * @Date: 2024-07-10 14:15:36
  * @LastEditors: Qmm 1259598502@qq.com
- * @LastEditTime: 2024-07-13 16:53:41
+ * @LastEditTime: 2024-07-13 20:59:48
  * @FilePath: \JSTS\ForSevMeowple\GamePage\Sudoku\sudoku\svelte.config.js
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -25,7 +25,7 @@ const config = {
 			precompress: false
 		}),
 		paths: {
-			base: '/Arklife'
+			base: '/Arkrun'
 		},
 		appDir: 'app'
 	}
